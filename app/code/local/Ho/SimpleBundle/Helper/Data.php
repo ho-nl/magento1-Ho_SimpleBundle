4922d269738acdc789d56c68cb2c75d75b9d920e
Paul Hachmang <paul@paul.local>
2014-04-17 21:28:29 +0200

    Better price rendering, works with dynamic pricing

--- app/code/local/Ho/SimpleBundle/Helper/Data.php
+++ app/code/local/Ho/SimpleBundle/Helper/Data.php
@@ -30,14 +30,38 @@ class Ho_SimpleBundle_Helper_Data extends Mage_Core_Helper_Abstract
      * @param Mage_Catalog_Model_Product $product
      * @return string
      */
-    public function getOriginalPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1)
+    public function getOriginalPriceHtml(Mage_Catalog_Model_Product $product)
     {
-        $price = $product->getPrice() * $qty;
+        $price = $this->_getOriginalPrice($product);
         return $this->_renderPrice($product, $price);
     }
 
+    protected function _getOriginalPrice(Mage_Catalog_Model_Product $product) {
+        if (! $product->getData('original_price')) {
+            $options = $this->getProductOptions($product);
+            $originalPrice = 0;
+            foreach($options as $option) {
+                $originalPrice += $option->getFinalPrice() * $option->getSelectionQty();
+            }
+            $product->setData('original_price', $originalPrice);
+        }
+
+        return  $product->getData('original_price');
+    }
+
+    public function getProductOptions(Mage_Catalog_Model_Product $product) {
+        /** @var Ho_SimpleBundle_Model_Bundle_Product_Type $typeInstance */
+        $typeInstance = $product->getTypeInstance(true);
+        return $typeInstance->getSelectionsCollection($typeInstance->getOptionsIds($product), $product);
+    }
+
     public function getDiscountPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1) {
-        $price = ($product->getPrice() - $product->getFinalPrice()) * $qty;
+        $price = ($this->_getOriginalPrice($product) - $product->getMaxPrice()) * $qty;
+        return $this->_renderPrice($product, $price);
+    }
+
+    public function getMaxPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1) {
+        $price = $product->getMaxPrice() * $qty;
         return $this->_renderPrice($product, $price);
     }
 
