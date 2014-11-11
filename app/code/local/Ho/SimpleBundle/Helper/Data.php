ee6d45c5520fd13e7f70868e06f821c11fdc7340
Paul Hachmang <paul@h-o.nl>
2014-11-11 16:54:37 +0100

    Implemented getDiscountPercentageHtml into helper

--- app/code/local/Ho/SimpleBundle/Helper/Data.php
+++ app/code/local/Ho/SimpleBundle/Helper/Data.php
@@ -85,41 +85,69 @@ class Ho_SimpleBundle_Helper_Data extends Mage_Core_Helper_Abstract
         return $this->_renderPrice($product, $price);
     }
 
-    protected function _getOriginalPrice(Mage_Catalog_Model_Product $product) {
-        if (! $product->getData('original_price')) {
+    protected function _getOriginalPrice(Mage_Catalog_Model_Product $product, $skipFirst = false)
+    {
+        $key = $skipFirst ? '_original_price_simple' : '_original_price';
+        if (! $product->getData($key)) {
             $options = $this->getProductOptions($product);
             $originalPrice = 0;
-            foreach($options as $option) {
+            foreach ($options as $option) {
+                if ($skipFirst) {
+                    $skipFirst = false;
+                    continue;
+                }
                 $originalPrice += $option->getFinalPrice() * $option->getSelectionQty();
             }
-            $product->setData('original_price', $originalPrice);
+            $product->setData($key, $originalPrice);
         }
 
-        return  $product->getData('original_price');
+        return  $product->getData($key);
     }
 
-    public function getProductOptions(Mage_Catalog_Model_Product $product) {
+
+    public function getProductOptions(Mage_Catalog_Model_Product $product)
+    {
         /** @var Ho_SimpleBundle_Model_Bundle_Product_Type $typeInstance */
         $typeInstance = $product->getTypeInstance(true);
         return $typeInstance->getSelectionsCollection($typeInstance->getOptionsIds($product), $product);
     }
 
-    public function getDiscountPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1) {
-        $price = ($this->_getOriginalPrice($product) - $product->getMaxPrice()) * $qty;
-        return $this->_renderPrice($product, $price);
+    public function getDiscountPercentageHtml(Mage_Catalog_Model_Product $product, $qty = 1)
+    {
+        return $this->_renderPercentage(
+            ($this->_getOriginalPrice($product, true) / 100) * $this->_getDiscountPrice($product) * 100
+        );
     }
 
-    public function getMaxPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1) {
+    public function getDiscountPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1)
+    {
+        return $this->_renderPrice($product, $this->_getDiscountPrice($product));
+    }
+
+    protected function _getDiscountPrice(Mage_Catalog_Model_Product $product, $qty = 1)
+    {
+        return ($this->_getOriginalPrice($product) - $product->getMaxPrice()) * $qty;
+    }
+
+    public function getMaxPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1)
+    {
         $price = $product->getMaxPrice() * $qty;
         return $this->_renderPrice($product, $price);
     }
 
-    public function getFinalPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1) {
+    public function getFinalPriceHtml(Mage_Catalog_Model_Product $product, $qty = 1)
+    {
         $price = $product->getFinalPrice() * $qty;
         return $this->_renderPrice($product, $price);
     }
 
-    protected function _renderPrice(Mage_Catalog_Model_Product $product, $price) {
+    protected function _renderPercentage($percentage)
+    {
+        return round($percentage).'%';
+    }
+
+    protected function _renderPrice(Mage_Catalog_Model_Product $product, $price)
+    {
         $store = $product->getStore();
         $convertedPrice = $store->roundPrice($store->convertPrice($price));
         $price = Mage::helper('tax')->getPrice($product, $convertedPrice);
